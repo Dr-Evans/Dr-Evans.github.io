7c95a146c9cedbe3c967303b29351008ad9ff8c1
Nick Evans <nickevans@ufl.edu>
2014-05-05 05:54:46 -0400

    inital commit

---
+++ scripts/init.js
@@ -0,0 +1,52 @@
+$(function() {
+	var themes;
+	var selectedThemeIndex;
+	var instructionsTimeout;
+	var deck;
+
+	function init() {
+		deck = bespoke.from('article');
+		initThemeSwitching();
+	}
+
+	init();
+
+	function initThemeSwitching() {
+		themes = [
+			'coverflow',
+			'classic',
+			'cube',
+			'carousel',
+			'concave'
+		];
+		
+		selectedThemeIndex = 0;
+
+		/*
+		left = 37
+		up = 38
+		right = 39
+		down = 40
+		*/
+		document.onkeydown = function(event){
+			if (event.keyCode == '38') {
+				selectedThemeIndex = modulo(selectedThemeIndex + 1, 4);
+				console.log("UP: " + selectedThemeIndex);
+				$("#body").removeAttr('class');
+				$("#body").addClass(themes[selectedThemeIndex]);
+			}
+			else if (event.keyCode == '40') {
+				selectedThemeIndex = modulo(selectedThemeIndex - 1, 4);
+				console.log("DOWN: " + selectedThemeIndex);
+				$("#body").removeAttr('class');
+				$("#body").addClass(themes[selectedThemeIndex]);
+			}
+		};
+		
+		$("#body").addClass(themes[selectedThemeIndex]);
+	}
+	
+	function modulo(num, n) {
+		return ((num % n) + n) % n;
+	}
+});
